b681b8d7a4c15c8e814286f34cd3fb0f223a35cf
Hwang <stafor@naver.com>
2017-05-24 13:25:24 +0900

    SeekActivity 레이아웃 & 코드 수정

--- app/src/main/java/com/stafor/gachonclass/SeekActivity.java
+++ app/src/main/java/com/stafor/gachonclass/SeekActivity.java
@@ -1,5 +1,6 @@
 package com.stafor.gachonclass;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,12 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import static com.stafor.gachonclass.R.array.building;
 
 public class SeekActivity extends AppCompatActivity {
     Button spreadBtn, spread2Btn;
+    Button[] floorBtns = new Button[15];
     LinearLayout layout;
     ImageView imageView;
     boolean[] isSpeard = new boolean[2];
+    TextView floorTv;
+    String floor = "1F";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +28,15 @@ public class SeekActivity extends AppCompatActivity {
         isSpeard[0] = false;
         isSpeard[1] = false;
 
-        layout = (LinearLayout) findViewById(R.id.layout_stairs);
+        floorTv = (TextView) findViewById(R.id.textView);
+        layout = (LinearLayout) findViewById(R.id.layout_floors);
         imageView = (ImageView) findViewById(R.id.imageView);
+
+        Intent myIntent = getIntent();
+        String building = myIntent.getStringExtra("building");
+        floorTv.setText(building + " " + floor + "F");
+        createFloors(building);
+
         spreadBtn = (Button) findViewById(R.id.btn_spread);
         spreadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +67,45 @@ public class SeekActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    public void createFloors(String building) {
+        if (building.equals("가천관"))
+            createButtons(-3, 7);
+        else if (building.equals("비전타워"))
+            createButtons(-3, 3);
+        else if (building.equals("공과대학1"))
+            createButtons(1, 5);
+        else if (building.equals("공과대학2"))
+            createButtons(1, 5);
+        else if (building.equals("바이오나노대학"))
+            createButtons(-2, 4);
+        else if(building.equals("공과대학2"))
+            createButtons(1, 6);
+    }
+
+    public void createButtons(int first, int last) {
+        int count = 0;
+        for (int i = first; i <= last; i++) {
+            floorBtns[count] = new Button(this);
+            if (i < 0) {
+                floorBtns[count].setText("B" + Integer.toString(-i));   // 지하
+            } else if (i == 0) {    // 0 처리
+                i++;
+                continue;
+            } else if (i > 0) { // 지상
+                floorBtns[count].setText(Integer.toString(i));
+            }
+            layout.addView(floorBtns[count]);
+            floorBtns[count].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    floor = ((Button)v).getText().toString();
+                    floorTv.setText(building + " " + floor + "F");
+                }
+            });
+            count++;
+        }
     }
 }
