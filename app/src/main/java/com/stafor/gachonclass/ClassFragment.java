ae4a6d9d8885d1a9a61467284a195f03fa7d687e
Hwang <stafor@naver.com>
2017-05-25 22:33:27 +0900

    정기적인 커밋

--- app/src/main/java/com/stafor/gachonclass/ClassFragment.java
+++ app/src/main/java/com/stafor/gachonclass/ClassFragment.java
@@ -1,28 +1,41 @@
 package com.stafor.gachonclass;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
-
-import java.util.ArrayList;
+import android.widget.Toast;
 
 public class ClassFragment extends Fragment {
     GridLayout layout;
-    ArrayList<Integer> list = new ArrayList<>();
+    String building, floor, classRoom;
     Button[] buttons;
+    AlertDialog.Builder builder;
+    final String[] items = {"수업정보 조회", "시간표 조회", "알림설정", "예약문의"};
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_class, container, false);
         layout = (GridLayout) rootView.findViewById(R.id.layout);
-
         init();
 
+        // 다이어로그 생성 밑 설정
+        builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(building + " " + floor);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(), items[which] + " 선택!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create();
         return rootView;
     }
 
@@ -30,13 +43,27 @@ public class ClassFragment extends Fragment {
         Bundle bundle = getArguments();
 
         if (bundle != null) {
-            list = bundle.getIntegerArrayList("list");
-            buttons = new Button[list.size()];
-        }
-        for (int i = 0; i < list.size(); i++) {
-            buttons[i] = new Button(getContext());
-            buttons[i].setText(Integer.toString(list.get(i)));
-            layout.addView(buttons[i]);
+            // 건물 이름과 층수를 받아온다.
+            building = bundle.getString("building");
+            floor = bundle.getString("floor");
+
+            buttons = new Button[13];
+            for (int i = 0; i < 13; i++) {
+                buttons[i] = new Button(getContext());
+                buttons[i].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        classRoom = ((Button)v).getText().toString();
+                        builder.setTitle(building + " "+ floor + "F " + classRoom + "호");
+                        builder.show();
+                    }
+                });
+                if (i  < 9) // 1 ~ 9 호
+                    buttons[i].setText(floor + "0" + (i+1));
+                else        // 10 ~ 호
+                    buttons[i].setText(floor + (i+1));
+                layout.addView(buttons[i]);
+            }
         }
     }
 }
--- app/src/main/java/com/stafor/gachonclass/RecentFragment.java
+++ app/src/main/java/com/stafor/gachonclass/RecentFragment.java
@@ -6,13 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class RecentFragment extends Fragment {
+    LinearLayout layout;
+    SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    String time;
+    ArrayList<Button> btnList = new ArrayList<>();
+    int count = 0;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_recent, container, false);
 
+        layout = (LinearLayout) rootView.findViewById(R.id.layout_btns);
+
+        addButton("가천관", "3", "302");
+        addButton("IT대학", "5", "502");
+
         return rootView;
+    }
+
+    public void addButton(String building, String floor, String classRoom) {
+        time = sdfNow.format(new Date(System.currentTimeMillis()));
+        Button button = new Button(getContext());
+        button.setText(building + " " + classRoom + "호 |" + time);
+        btnList.add(button);
+        layout.addView(btnList.get(count++));
     }
 }
